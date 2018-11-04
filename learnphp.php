d6c1114e6114606bad602d295c3ccae9342f49db
Dennis Onder <webdev.dennisonder@gmail.com>
2018-11-04 18:07:14 +0100

    Basic PHP

---
+++ learnphp.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>PHP Information Gathered:</title>
+</head>
+<body>
+  
+  <?php
+    echo "<p>Data Processed</p>";
+    date_default_timezone_set('UTC');
+    echo date('h:i:s a, l F jS Y e');
+  ?>
+
+</body>
+</html>
