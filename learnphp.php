3b544c603828aba6dfb755d75609efe5fbace978
Dennis Onder <webdev.dennisonder@gmail.com>
2018-11-04 19:09:18 +0100

    If/Else statements

--- learnphp.php
+++ learnphp.php
@@ -9,13 +9,14 @@
     $addressName = $_POST['address'];
     $cityName = $_POST['city'];
 
+    echo '<p>Customer Info:</p>';
     echo $usersName . "<br />";
     echo $addressName . "<br />";
     echo $cityName . "<br />";
 
     echo "<br />";
 
-    $dataString = 
+    $message = 
     <<<EOD
       The customers name is 
       $usersName, 
@@ -23,10 +24,27 @@
       $addressName, $cityName<br />
 EOD;
 
-    echo $dataString;
+    echo $message;
 
     define('PI', 3.14);
     echo PI;
+
+    echo "<br />";
+
+    if(5 == 10) {
+      echo '<p>True</p>';
+    } else {
+      echo '<p>5 is not equal to 10</p>';
+    };
+
+    $numOfOranges = 4;
+    $numOfBananas = 36;
+
+    if($numOfOranges > 25 || $numOfBananas > 30) {
+      echo '<p>25% Discount!</p>';
+    } else {
+      echo '<p>No Discount</p>';
+    };
   ?>
 
 </body>
