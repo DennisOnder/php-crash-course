b9eb0497a051ac1dae1fd24d6fff3e79eb6b6a38
Dennis Onder <webdev.dennisonder@gmail.com>
2018-11-04 18:51:13 +0100

    EOD Strings and constants

--- learnphp.php
+++ learnphp.php
@@ -9,9 +9,24 @@
     $addressName = $_POST['address'];
     $cityName = $_POST['city'];
 
-    echo $usersName . "</br>";
-    echo $addressName . "</br>";
-    echo $cityName . "</br>";
+    echo $usersName . "<br />";
+    echo $addressName . "<br />";
+    echo $cityName . "<br />";
+
+    echo "<br />";
+
+    $dataString = 
+    <<<EOD
+      The customers name is 
+      $usersName, 
+      and the customer lives in 
+      $addressName, $cityName<br />
+EOD;
+
+    echo $dataString;
+
+    define('PI', 3.14);
+    echo PI;
   ?>
 
 </body>
